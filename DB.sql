a37146e7e85e3cf11c797bd5542b117310287b65
Dex <enriquesaenz0@gmail.com>
2023-05-31 21:35:48 -0500

    add: database

---
+++ DB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE banner (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  img varchar(100) NOT NULL,
+  fecha TIMESTAMP
+);
+
+INSERT into banner(img) values ("vistas/img/banner/banner01.jpg");
+INSERT into banner(img) values ("vistas/img/banner/banner02.jpg");
+INSERT into banner(img) values ("vistas/img/banner/banner03.jpg");
+INSERT into banner(img) values ("vistas/img/banner/banner04.jpg");
